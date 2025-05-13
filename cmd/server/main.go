e28720f3501d3bfb6186f49d60887c227d77043b
José C. Martins <zemartins81@gmail.com>
2025-05-13 15:08:36 -0300

    "Iniciando com o exemplo do Huncoding: https://youtu.be/P1ZK1F8Xs9M?si=8MxP7eIYG4LGPECF"

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,80 @@
 package main
 
+import (
+	"context"
+	"fmt"
+	"net"
+	"net/http"
+	"time"
+
+	"github.com/go-redis/redis/v8"
+	"golang.org/x/net/context"
+)
+
+type RateLimiter struct {
+	client  *redis.Client
+	limit   int
+	window  time.Duration
+	context context.Context
+}
+
+func NewRatelimiter(
+	client *redis.Client,
+	limit int,
+	window time.Duration,
+) (rateLimiter *RateLimiter) {
+	return &RateLimiter{
+		client:  client,
+		limit:   limit,
+		window:  window,
+		context: context.Background(),
+	}
+
+}
+
+func (rl *RateLimiter) Allow(key string) bool {
+	pipe := rl.client.TxPipeline()
+
+	incr := pipe.Incr(rl.context, key)
+	pipe.Expire(rl.context, key, rl.window)
+
+	cmder, err := pipe.Exec(rl.context)
+	if err != nil {
+		panic(err)
+	}
+
+	return incr.Val() <= int64(rl.limit)
+
+}
+
+func rateLimiterMd(rl *RateLimiter, next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		clientIP, _, _ := net.SplitHostPort(r.RemoteAddr)
+		if !rl.Allow(clientIP) {
+			http.Error(w, "Too many requests", http.StatusTooManyRequests)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
+}
+
 func main() {
 
-	//carregar configuração
-	config, err := 
+	client := redis.NewClient(&redis.Options{
+		Addr: "localhost:6379",
+	})
+	defer client.Close()
+
+	rateLimiter := NewRatelimiter(client, 10, 1*time.Minute)
+
+	router := http.NewServeMux()
+	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello World!")
+	})
+
+	handler := rateLimiterMd(rateLimiter, router)
+
+	http.ListenAndServe(":8080", handler)
+
 }
